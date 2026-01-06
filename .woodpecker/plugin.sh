135f4b3df9e3d0a1bb42103e56bb9abffafb3c71
Yukari Hafner <shinmera@tymoon.eu>
2026-01-06 14:58:06 +0100

    Docker fumbling

--- .woodpecker/plugin.sh
+++ .woodpecker/plugin.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 IFS=', ' read -r -a systems <<< "${PLUGIN_SYSTEMS:-${CI_REPO_NAME:-$@}}"
-IFS=', ' read -r -a tests <<< "${PLUGIN_TESTS:-${CI_REPO_NAME:-$@}}"
+IFS=', ' read -r -a tests <<< "${PLUGIN_TESTS:-${systems[@]}}"
 readonly IMPLEMENTATION="${PLUGIN_IMPLEMENTATION:-sbcl}"
 
-exec /usr/local/bin/cl-all "$IMPLEMENTATION" -ni \
+exec /usr/local/bin/cl-all "$IMPLEMENTATION" -vni \
      -l "${HOME}/.quicklisp/setup.lisp" \
      -e "(asdf:initialize-source-registry (list :source-registry :ignore-inherited-configuration (list :tree (uiop:parse-native-namestring \"$(pwd)/\"))))" \
      -e "(ql:quickload '(parachute ${systems[@]}))" \
--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,9 @@
 FROM shinmera/cl-all
+RUN cl-all sbcl -pil "${HOME}/.quicklisp/setup.lisp" \
+              -e "(ql:update-all-dists :prompt NIL)" \
+              -e "(ql:quickload :parachute)" \
+    && cl-all -xpil "${HOME}/.quicklisp/setup.lisp" \
+              -e "(ql:quickload :parachute)"
 COPY .woodpecker/plugin.sh /usr/local/bin/plugin.sh
-RUN cl-all -pil "${HOME}/.quicklisp/setup.lisp" -e "(ql:quickload :parachute)" \
-    && cl-all -xpil "${HOME}/.quicklisp/setup.lisp" -e "(ql:quickload :parachute)" \
-    && sudo chmod +x /usr/local/bin/plugin.sh
-
+RUN sudo chmod +x /usr/local/bin/plugin.sh
 ENTRYPOINT ["/usr/local/bin/plugin.sh"]
